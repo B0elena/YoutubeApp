d380dda2e642aaaea79ab9be4a81ea1ddb801e0d
B0elena <e.mc2.1012md@gmail.com>
2021-02-28 21:49:56 +0900

    enumで表記を直す

--- YoutubeApp/ViewControllers/BaseTabBarController.swift
+++ YoutubeApp/ViewControllers/BaseTabBarController.swift
@@ -8,26 +8,37 @@
 import UIKit
 
 class BaseTabBarController: UITabBarController {
+    // enumで設定<
+    enum ControllerName: Int {
+        case home, search, channel, inbox, library
+    }
+    // enumで設定>
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViewControllers()
+    }
+    
+    private func setupViewControllers() {
         viewControllers?.enumerated().forEach({ (index, viewController) in
-            switch index {
-            case 0:
-                setTabbarInfo(viewController, selectedImageName: "家の無料アイコン (1)", unselectedImageName: "家の無料アイコン", title: "ホーム")
-            case 1:
-                setTabbarInfo(viewController, selectedImageName: "方位磁針のアイコン素材2 (1)", unselectedImageName: "方位磁針のアイコン素材2", title: "検索")
-            case 2:
-                setTabbarInfo(viewController, selectedImageName: "再生ボタン付きのカチンコアイコン2 (1)", unselectedImageName: "再生ボタン付きのカチンコアイコン2", title: "登録チャンネル")
-            case 3:
-                setTabbarInfo(viewController, selectedImageName: "メールの無料アイコン (1)", unselectedImageName: "メールの無料アイコン", title: "受信トレイ")
-            case 4:
-                setTabbarInfo(viewController, selectedImageName: "積み重ねた本のアイコン素材 (1)", unselectedImageName: "積み重ねた本のアイコン素材", title: "ライブラリ")
-            default:
-                break
+            if let name = ControllerName.init(rawValue: index) {
+                switch name {
+                case .home:
+                    setTabbarInfo(viewController, selectedImageName: "家の無料アイコン (1)", unselectedImageName: "家の無料アイコン", title: "ホーム")
+                case .search:
+                    setTabbarInfo(viewController, selectedImageName: "方位磁針のアイコン素材2 (1)", unselectedImageName: "方位磁針のアイコン素材2", title: "検索")
+                case .channel:
+                    setTabbarInfo(viewController, selectedImageName: "再生ボタン付きのカチンコアイコン2 (1)", unselectedImageName: "再生ボタン付きのカチンコアイコン2", title: "登録チャンネル")
+                case .inbox:
+                    setTabbarInfo(viewController, selectedImageName: "メールの無料アイコン (1)", unselectedImageName: "メールの無料アイコン", title: "受信トレイ")
+                case .library:
+                    setTabbarInfo(viewController, selectedImageName: "積み重ねた本のアイコン素材 (1)", unselectedImageName: "積み重ねた本のアイコン素材", title: "ライブラリ")
+                }
             }
         })
     }
+    
     private func setTabbarInfo(_ viewController: UIViewController, selectedImageName: String, unselectedImageName: String, title: String) {
         viewController.tabBarItem.selectedImage = UIImage(named: selectedImageName)?.resize(size: .init(width: 20, height: 20))?.withRenderingMode(.alwaysOriginal)
         viewController.tabBarItem.image = UIImage(named: unselectedImageName)?.resize(size: .init(width: 20, height: 20))?.withRenderingMode(.alwaysOriginal)
