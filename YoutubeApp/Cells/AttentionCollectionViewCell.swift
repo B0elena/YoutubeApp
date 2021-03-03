6d0596d304e0a28c79c877a6d0393f2dfc4bc07a
B0elena <e.mc2.1012md@gmail.com>
2021-03-03 12:20:20 +0900

    横スクロールのセルに情報を反映

--- YoutubeApp/Cells/AttentionCollectionViewCell.swift
+++ YoutubeApp/Cells/AttentionCollectionViewCell.swift
@@ -6,16 +6,31 @@
 //
 
 import UIKit
+import Nuke
 
+// 横スクロールのセルに反映させる<
 class AttentionCollectionViewCell: UICollectionViewCell {
-    
-    
-    @IBOutlet weak var thumbnailImageView: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        backgroundColor = .purple
+    var videoItem: Item? {
+        didSet {
+            //サムネイルの読み込みをして反映<
+            if let url = URL(string: videoItem?.snippet.thumbnails.medium.url ?? "") {
+                Nuke.loadImage(with: url, into: thumbnailImageView)
+            }
+            //サムネイルの読み込みをして反映>
+            videoTitleLabel.text = videoItem?.snippet.channelTitle
+            discriptionLabel.text = videoItem?.snippet.description
+            channelTitleLabel.text = videoItem?.channel?.items[0].snippet.title
+        }
     }
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var videoTitleLabel: UILabel!
+    @IBOutlet weak var channelTitleLabel: UILabel!
+    @IBOutlet weak var discriptionLabel: UILabel!
+    // ジブを紐付けるための設定<
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    // ジブを紐付けるための設定>
 }
+// 横スクロールのセルに反映させる>
